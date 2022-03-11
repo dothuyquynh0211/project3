eb5c8a9eb80ad48ac3d853704471b303662d9bee
Do Thuy Quynh <dothuyquynh0211@gmail.com>
2022-03-11 16:08:24 +0700

    template customer

---
+++ database/migrations/2022_03_08_022146_create_admin_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name');
+            $table->string('phone',20);
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->unsignedBigInteger('id_role');
+            $table->foreign('id_role')->references('id')->on('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+use App\Http\Controllers\Admin\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +16,48 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Đường dẫn trang admin - trước mỗi đường dẫn thêm tiền tố admin để phân biết với bên khách hàng 
 
+
+Route::get('/admin', function () {
+    return redirect()->route('admin.home');
+});
+
+Route::get('/admin/home', [AdminController::class, 'index'])->name('admin.home');
+
+Route::get('/admin/login', [LoginController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/login', [LoginController::class, 'login']);
+//Route::get('/register',[LoginController::class,'register'])->name('register');
+Route::get('/admin/logout', [LoginController::class, 'logout'])->name('admin.logout');
+
+
+
+
+// Đường dẫn trang khách hàng 
 Route::get('/', function () {
-    return view('index');
+    return view('frontend.index');
+});
+Route::get('/checkout', function () {
+    return view('frontend.checkout');
+});
+Route::get('/contact', function () {
+    return view('frontend.contact');
+});
+Route::get('/shop', function () {
+    return view('frontend.shop');
+});
+Route::get('/cart', function () {
+    return view('frontend.shop_cart');
+});
+Route::get('/cart', function () {
+    return view('frontend.shop_cart');
+});
+Route::get('/product-detail', function () {
+    return view('frontend.product_details');
+});
+Route::get('/blog', function () {
+    return view('frontend.blog');
+});
+Route::get('/blog-detail', function () {
+    return view('frontend.blog_details');
 });
