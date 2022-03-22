cc478d0130e6e5c016aadce4d9a341365e0be87d
thuydat <dieuthuy14032001@gmail.com>
2022-03-22 18:52:31 +0700

    abc

--- resources/views/frontend/shop_cart.blade.php
+++ resources/views/frontend/shop_cart.blade.php
@@ -20,6 +20,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
+                     <?php 
+                     
+                    $content=Cart::content();
+                    // echo '<pre>';
+                    //     print_r($content);
+                    //     echo '</pre>';
+                        ?> 
                     <div class="shop__cart__table">
                         <table>
                             <thead>
@@ -32,115 +39,65 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                @foreach ($content as $v_content)
                                 <tr>
                                     <td class="cart__product__item">
-                                        <img src="img/shop-cart/cp-1.jpg" alt="">
+                                        <img src="/image/{{$v_content->options->image}}" alt="" style="max-height:100px; max-width:100px">
                                         <div class="cart__product__item__title">
-                                            <h6>Chain bucket bag</h6>
+                                            <h6> {{$v_content->name}}</h6>
+                                            <h6> {{$v_content->sku}}</h6>  
                                             <div class="rating">
                                                 <i class="fa fa-star"></i>
                                                 <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="cart__price">$ 150.0</td>
+                                                <i class="fa fa-star"></i></td>
+                            
+                                    <td class="cart__price">{{number_format( $v_content->price)}}VNĐ</td>
+                                  
                                     <td class="cart__quantity">
+                                        <form method="POST" action="/updateCart">
                                         <div class="pro-qty">
-                                            <input type="text" value="1">
+                                            <input type="text" value="{{$v_content->qty}}" name="quantity">
+                                            <input type="hidden" value="{{$v_content->rowId}}" name="rowId_cart" class="form-control">
+                                          {{-- //  <button type="submit" name="update_qty" value="cập nhật"><span class="icon_loading"></span> </button> --}}
                                         </div>
+                                        </form>
                                     </td>
-                                    <td class="cart__total">$ 300.0</td>
-                                    <td class="cart__close"><span class="icon_close"></span></td>
+                                    <td class="cart__total">
+                                        <?php 
+                                        $subtotal= $v_content->price*$v_content->qty;
+                                        echo number_format($subtotal).''.'VND';
+                                        ?>
+                                    </td>
+                                    <td class="cart__close"><a href="/deleteCart/{{$v_content->rowId}}"><span class="icon_close">
+                                        </span></a></td>
                                 </tr>
-                                <tr>
-                                    <td class="cart__product__item">
-                                        <img src="img/shop-cart/cp-2.jpg" alt="">
-                                        <div class="cart__product__item__title">
-                                            <h6>Zip-pockets pebbled tote briefcase</h6>
-                                            <div class="rating">
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="cart__price">$ 170.0</td>
-                                    <td class="cart__quantity">
-                                        <div class="pro-qty">
-                                            <input type="text" value="1">
-                                        </div>
-                                    </td>
-                                    <td class="cart__total">$ 170.0</td>
-                                    <td class="cart__close"><span class="icon_close"></span></td>
-                                </tr>
-                                <tr>
-                                    <td class="cart__product__item">
-                                        <img src="img/shop-cart/cp-3.jpg" alt="">
-                                        <div class="cart__product__item__title">
-                                            <h6>Black jean</h6>
-                                            <div class="rating">
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="cart__price">$ 85.0</td>
-                                    <td class="cart__quantity">
-                                        <div class="pro-qty">
-                                            <input type="text" value="1">
-                                        </div>
-                                    </td>
-                                    <td class="cart__total">$ 170.0</td>
-                                    <td class="cart__close"><span class="icon_close"></span></td>
-                                </tr>
-                                <tr>
-                                    <td class="cart__product__item">
-                                        <img src="img/shop-cart/cp-4.jpg" alt="">
-                                        <div class="cart__product__item__title">
-                                            <h6>Cotton Shirt</h6>
-                                            <div class="rating">
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                                <i class="fa fa-star"></i>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="cart__price">$ 55.0</td>
-                                    <td class="cart__quantity">
-                                        <div class="pro-qty">
-                                            <input type="text" value="1">
-                                        </div>
-                                    </td>
-                                    <td class="cart__total">$ 110.0</td>
-                                    <td class="cart__close"><span class="icon_close"></span></td>
-                                </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
+                    
                 </div>
-            </div>
+                    
+                
+            </div> 
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="cart__btn">
                         <a href="#">Continue Shopping</a>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-6">
+                {{-- <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="cart__btn update__btn">
-                        <a href="#"><span class="icon_loading"></span> Update cart</a>
+                     <button type="submit" name="update_qty" value="cập nhật"><span class="icon_loading"></span> Update cart</button>
                     </div>
-                </div>
+                </div> --}}
             </div>
+
+
+
+
+
+
             <div class="row">
                 <div class="col-lg-6">
                     <div class="discount__content">
@@ -155,14 +112,16 @@
                     <div class="cart__total__procced">
                         <h6>Cart total</h6>
                         <ul>
-                            <li>Subtotal <span>$ 750.0</span></li>
-                            <li>Total <span>$ 750.0</span></li>
+                            <li>Subtotal <span>0</span></li>
+                            <li>Total <span>{{Cart::total().''.'VND'}}</span></li>
                         </ul>
-                        <a href="#" class="primary-btn">Proceed to checkout</a>
+                        <a href="frontend.checkout/" class="primary-btn">Proceed to checkout</a>
                     </div>
                 </div>
             </div>
         </div>
+    </section> 
+        </div>
     </section>
-    <!-- Shop Cart Section End -->
+ 
 @endsection
