990eb8ae22407eafc5e00295e6883e672355c8e0
thuydat <dieuthuy14032001@gmail.com>
2022-04-14 14:48:06 +0700

    abc

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -1,3 +1,24 @@
+<?php 
+$connect= new mysqli('localhost','root','','project3');
+$query =" SELECT products.*,(sum_import.quantity_import - sum_invoice.quantity_invoice ) 
+        AS product_inventory , sum_invoice.quantity_invoice ,sum_import.quantity_import 
+        FROM (
+            SELECT sum(quantity) AS quantity_invoice , id_product FROM `invoice_details` GROUP BY id_product
+            ) AS sum_invoice
+        JOIN(
+            SELECT sum(quantity) AS quantity_import , id_product FROM `import_details` GROUP BY id_product
+            ) AS sum_import ON sum_invoice.id_product = sum_import.id_product 
+        JOIN products ON sum_import.id_product = products.id";
+$result=mysqli_query($connect,$query);
+$data=[];
+while ($row = mysqli_fetch_array($result)){
+    $data[]=$row;
+   
+}
+// echo "<pre>";
+//     var_dump($data);
+//     echo "</pre>";
+?>
 @extends('backend.layout.master')
 @section('title', 'Trang chủ Admin')
 
@@ -186,7 +207,44 @@
         </section>
 
     </div>
-    
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+            ['nameb','number_book'],
+            <?php 
+
+          foreach ($data as $key ) {
+          echo "['". $key['name']."' , ". $key['quantity_import'] . ",  ],";
+          }
+
+         ?> 
+          
+        ]);
+
+        var options = {
+          title: 'Biểu đồ số lượng sách được mượn'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+     
+    </script>
+  
+    <div id="piechart" style="width: 1000px; height: 600px;margin:auto"></div>
+
+
+
+   
+
+  
+
 
 @endsection
 
